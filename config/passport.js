970e959da0111a344ac92e2087caa1dbfe76d069
Abraham3001 <116362230+Abraham3001@users.noreply.github.com>
2025-06-30 18:52:27 -0500

    passport.js

--- config/passport.js
+++ config/passport.js
@@ -2,40 +2,44 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const Usuario = require('../models/usuario');
 
-passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: process.env.GOOGLE_CALLBACK_URL
-},
-async (accessToken, refreshToken, profile, done) => {
-  try {
-    let usuario = await Usuario.findOne({ where: { googleId: profile.id } });
-    if (!usuario) {
-      usuario = await Usuario.create({
-        nombre: profile.displayName,
-        email: profile.emails[0].value,
-        googleId: profile.id,
-        rol: 'estudiante'
-      });
+module.exports = function(passport) {
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await Usuario.findByPk(id);
+      done(null, user);
+    } catch (err) {
+      done(err, null);
     }
-    return done(null, usuario);
-  } catch (err) {
-    console.error("Error en login con Google:", err);
-    return done(err, null);
-  }
-}));
+  });
 
-passport.serializeUser((usuario, done) => {
-  done(null, usuario.id);
-});
+  passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
+  },
+  async (accessToken, refreshToken, profile, done) => {
+    try {
+      let user = await Usuario.findOne({ where: { email: profile.emails[0].value } });
 
-passport.deserializeUser(async (id, done) => {
-  try {
-    const usuario = await Usuario.findByPk(id);
-    done(null, usuario);
-  } catch (error) {
-    done(error, null);
-  }
-});
-
+      if (user) {
+        return done(null, user);
+      } else {
+        user = await Usuario.create({
+          nombre: profile.displayName,
+          email: profile.emails[0].value,
+          password: null, // IMPORTANTE: acepta NULL en tu modelo
+          rol: 'estudiante'
+        });
+        return done(null, user);
+      }
+    } catch (err) {
+      console.error('Error en GoogleStrategy:', err);
+      return done(err, null);
+    }
+  }));
+};
 module.exports = passport;
